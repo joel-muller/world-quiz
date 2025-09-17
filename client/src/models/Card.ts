3f4a228a03fd92fbaac94888fa201bf21ac816bf
Joel Müller <joelmueller27@gmail.com>
2025-09-17 09:44:20 +0200

    chore - clean repository for angular setup

--- client/src/models/Card.ts
+++
@@ -1,7 +0,0 @@
-export interface Card {
-  PlaceId: number
-  Front: string
-  Back: string
-  InfoFront?: string
-  InfoBack?: string
-}
--- client/src/models/Game.ts
+++
@@ -1,7 +0,0 @@
-import  { type Card } from '@/models/Card.ts'
-
-export interface Game {
-  Id: number
-  Category: number
-  Cards: Card[]
-}
