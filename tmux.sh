9ab85d2e47099fdfb0b3e5686af098780ef07eb7
Joel Müller <joelmueller27@gmail.com>
2025-10-21 19:59:28 +0200

    chore - remove autoamtically open nvim

--- tmux.sh
+++ tmux.sh
@@ -8,9 +8,10 @@ tmux new-window -t "$SESSION" -n server-cli1
 tmux new-window -t "$SESSION" -n server-cli2
 tmux new-window -t "$SESSION" -n notes
 
-tmux send-keys -t "$SESSION":server-nvim "cd ~/world-quiz/server && nvim" C-m
+tmux send-keys -t "$SESSION":server-nvim "cd ~/world-quiz/server" C-m
 tmux send-keys -t "$SESSION":server-cli1 "cd ~/world-quiz/server" C-m
 tmux send-keys -t "$SESSION":server-cli2 "cd ~/world-quiz/server" C-m
-tmux send-keys -t "$SESSION":notes "cd ~/notes && nvim" C-m
+tmux send-keys -t "$SESSION":client-cli1 "cd ~/world-quiz/client" C-m
+tmux send-keys -t "$SESSION":notes "cd ~/notes" C-m
 
 tmux attach -t "$SESSION"
