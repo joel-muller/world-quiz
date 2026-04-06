a41437c993e0d3600aa7aa91497bb92e1b9a2e11
Joel Müller <joelmueller27@gmail.com>
2026-04-06 12:39:20 +0200

    build - add cors settings to client and remove sonar

--- server/src/main/java/com/worldquiz/controller/Controller.java
+++ server/src/main/java/com/worldquiz/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://world-quiz.org"})
 @RequestMapping("/api/quiz")
 @RequiredArgsConstructor
 public class Controller {
