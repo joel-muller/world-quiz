a1670ce3c1100f3507c626993bfcaaa31b13e1b1
Joel Müller <joelmueller27@gmail.com>
2026-04-06 12:17:13 +0200

    build - ajust the paths for comunication

--- server/src/main/java/com/worldquiz/controller/Controller.java
+++ server/src/main/java/com/worldquiz/controller/Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/quiz")
+@RequestMapping("/api/quiz")
 @RequiredArgsConstructor
 public class Controller {
     private final GameService gameService;
